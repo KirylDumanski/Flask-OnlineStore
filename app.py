4ea3257831c09b38e8bc5bd2604684011e7424c5
Kiryl Dumanski <Kocherizhkin@gmail.com>
2022-12-10 21:35:38 +0300

    Feat: Add session cart

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import uuid
 from datetime import datetime
 
-from flask import Flask, render_template, flash, request, redirect, url_for
+from flask import Flask, render_template, flash, request, redirect, url_for, session
 from flask_login import UserMixin, LoginManager, current_user, login_user, login_required, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -10,8 +10,9 @@ from slugify import slugify
 from sqlalchemy.exc import NoResultFound
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
-from wtforms import SubmitField, StringField, IntegerField, BooleanField, TextAreaField, FileField, PasswordField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms import SubmitField, StringField, IntegerField, BooleanField, TextAreaField, FileField, PasswordField, \
+    SelectField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, InputRequired, NumberRange
 from wtforms_alchemy import QuerySelectField
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -22,8 +23,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'st
 app.config['UPLOAD_FOLDER'] = os.path.join(basedir, 'static\\images\\')
 login_manager = LoginManager(app)
 db = SQLAlchemy(app)
+CART_SESSION_ID = 'cart'
 
 
+# Models
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False, unique=True)
@@ -59,11 +62,6 @@ class Category(db.Model):
         return f"{self.name}"
 
 
-@app.context_processor
-def inject_sidebar():
-    return dict(sidebar_categories=Category.query.order_by(Category.name))
-
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True)
@@ -81,6 +79,22 @@ def user_loader(user_id):
     return User.query.get(user_id)
 
 
+# Context processors
+@app.context_processor
+def inject_sidebar():
+    return dict(sidebar_categories=Category.query.order_by(Category.name))
+
+
+@app.context_processor
+def inject_cart_total_quantity():
+    try:
+        total_quantity = sum(item['quantity'] for item in session['cart'].values())
+        return dict(total_quantity=total_quantity)
+    except:
+        return dict(total_quantity=0)
+
+
+# Forms
 class RegisterForm(FlaskForm):
     first_name = StringField("First name: ", validators=[DataRequired()])
     last_name = StringField("Last name: ", validators=[DataRequired()])
@@ -116,10 +130,18 @@ class CategoryForm(FlaskForm):
     submit = SubmitField("Add category")
 
 
+class CartAddProductForm(FlaskForm):
+    quantity = SelectField(coerce=int, validators=[InputRequired(), NumberRange(min=1)],
+                           choices=[('1', 1), ('2', 2), ('3', 3), ('4', 4), ('5', 5)])
+    submit = SubmitField('Add to cart')
+
+
+# Views
 @app.route('/category/<string:category_slug>')
 @app.route('/')
 def index(category_slug=None):
     products = Product.query.filter(Product.active == 1)
+    cart_add_form = CartAddProductForm()
     if category_slug:
         try:
             products = Product.query.filter(Product.active == 1).join(Category).filter(Category.slug == category_slug)
@@ -128,7 +150,9 @@ def index(category_slug=None):
             return render_template('store/index.html', empty_query=True)
         except Exception as e:
             print(e)
-    return render_template('store/index.html', products=products)
+    return render_template('store/index.html',
+                           products=products,
+                           form=cart_add_form)
 
 
 @app.route('/product/add', methods=["GET", "POST"])
@@ -263,6 +287,96 @@ def dashboard():
                            form=form,
                            user_to_update=user_to_update,
                            user_id=user_id)
+
+
+class Cart:
+    def __init__(self, session):
+        self.session = session
+        cart = session.get(CART_SESSION_ID)
+        if not cart:
+            self.session[CART_SESSION_ID] = {}
+            cart = self.session[CART_SESSION_ID]
+        self.cart = cart
+
+    def add(self, product: Product, quantity=1, update_quantity=False):
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': product.price}
+
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+
+        self.save()
+
+    def save(self):
+        self.session.modified = True
+
+    def remove(self, product: Product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(self):
+        product_ids = list(map(int, self.cart.keys()))
+
+        products = Product.query.filter(Product.id.in_(product_ids))
+
+        for product in products:
+            self.cart[str(product.id)]['product'] = product
+
+        for item in self.cart.values():
+            item['total_price'] = round(int(item['price']) / 100 * int(item['quantity']), 2)  # in cents
+            yield item
+
+    def __len__(self):
+        """Counting all items in the cart."""
+        try:
+            return sum(item['quantity'] for item in self.cart.values())
+        except AttributeError:
+            return 0
+
+    def get_total_price(self):
+        """Calculate the cost of items in the shopping cart."""
+        return round(sum(int(item['price']) / 100 * int(item['quantity']) for item in self.cart.values()), 2)
+
+    def clear(self):
+        """ Delete cart from session"""
+        del self.session[CART_SESSION_ID]
+        self.session.modified = True
+
+
+@app.route('/cart/add/', methods=['POST'])
+def cart_add():
+    if request.method == 'POST':
+        cart = Cart(session)
+        product_id = request.form['product_id']
+        product = Product.query.get_or_404(int(product_id))
+        cart.add(product=product,
+                 quantity=int(request.form['quantity']),
+                 update_quantity=bool(request.form['update_quantity']))
+
+        return redirect(url_for('cart_detail'))
+
+
+@app.route('/cart/remove/', methods=['POST'])
+def cart_remove():
+    cart = Cart(session)
+    product_id = request.form['product_id']
+    product = Product.query.get_or_404(int(product_id))
+    cart.remove(product)
+    return redirect(url_for('cart_detail'))
+
+
+@app.route('/cart/detail/', methods=['GET', 'POST'])
+def cart_detail():
+    cart = Cart(session)
+    form = ProductForm()
+    return render_template('store/cart_detail.html', cart=cart, form=form)
 
 
 if __name__ == '__main__':
